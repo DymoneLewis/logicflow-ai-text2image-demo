49cbf9e3d1f6aa99755cd1329164cf5d7010a494
liuziqi <liuziqi@didiglobal.com>
2024-11-15 16:43:35 +0800

    fix: 增加deploy命令, 增加不上传dist的配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/logicflow-vue2-demo/" : "/",
   devServer: {
     proxy: {
       "/ali": {
